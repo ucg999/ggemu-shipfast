c961780ad5440aac5731f3e5b34f9440b5ec5f76
ucg999 <94113263@qq.com>
2026-08-23 15:42:19 +0800

    fix: redirect root and bind www domain

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,22 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 import { normalizeSiteTemplate } from '#/lib/site-config'
+import { SITE_ORIGIN } from '#/lib/site-url'
 
 export const Route = createFileRoute('/')({
   validateSearch: (search) => ({
     template: normalizeSiteTemplate(search.template),
   }),
   beforeLoad: ({ search }) => {
+    const target = new URL('/zh-CN', SITE_ORIGIN)
+
+    if (search.template) {
+      target.searchParams.set('template', search.template)
+    }
+
     throw redirect({
-      params: { locale: 'zh-CN' },
-      search,
-      to: '/$locale',
+      href: target.toString(),
+      statusCode: 301,
     })
   },
 })
